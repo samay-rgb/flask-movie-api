a9e87016e849a2a413e952032a846df802755297
Saksham-1508 <saksham15802@gmail.com>
2021-12-18 10:38:00 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from typing import Mapping
+from flask import Flask, jsonify
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import difflib
+df = pd.read_csv('final_movies.csv')
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/movielist/<string:imdb_id>")
+def get_movie_list(imdb_id):
+    df = pd.read_csv('final_movies.csv')
+    
+    d = (df.loc[df['imdb_title_id']==imdb_id]).values
+    if(len(d)==0):
+        return jsonify(None)
+    result ={}
+    for i,j in zip(df,d[0]):
+        if i=="Unnamed: 0":
+            i = 'index'
+        result[i] = j
+    return jsonify(result)
+
+@app.route("/similarity/<string:imdb_id>")
+def get_movie_similarity(imdb_id):
+    vectorizer = TfidfVectorizer()
+    feature_vectors = vectorizer.fit_transform(df['comb'])
+    similarity = cosine_similarity(feature_vectors)
+    name = (df.loc[df['imdb_title_id']==imdb_id]).original_title.values[0]
+    closest_index = df[df['original_title'] == name].index.values[0]
+    similarity_score = list(enumerate(similarity[closest_index]))
+    # print(similarity_score)
+    sorted_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    # print(sorted_score)
+    result = []
+    for i in range(1, 11):
+        idx = sorted_score[i][0]
+        # print(idx)de
+        result.append({'title':df['original_title'][idx],'index':idx,'imdb_id':df['imdb_title_id'][idx]})
+    return jsonify(result)
+if __name__ == "__main__":
+    app.run(debug=True)
